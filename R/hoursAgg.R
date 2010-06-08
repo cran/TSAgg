af6d94bb5e91293846ad2914f6ac08fcbfefb4b4
Jason Scott Lessels <jason.lessels@sydney.edu.au>
2010-06-08 00:00:00 +0000

    version 0.1-7

---
+++ R/hoursAgg.R
@@ -0,0 +1,36 @@
+hoursAgg<-
+	function(data,
+			process,
+			multiple=NULL,
+			na.rm=FALSE){
+
+		
+          if(!is.null(multiple)){
+          	if(multiple>1){
+            dates <- as.POSIXct(strptime(paste(data$hour,data$day,data$month,data$year),"%H %d %m %Y"))
+            ## Get the gep between each hour.
+            gap <- as.numeric(difftime(dates[2:length(dates)],dates[1:length(dates)-1]),units="hours")
+            cum.gap <- gap
+            for(i in 2:length(cum.gap)){
+              cum.gap[i]=cum.gap[i-1]+cum.gap[i]+data$hour[1]
+            }
+            cum.gap <- c(data$hour[1],cum.gap)
+            data$hour<-cum.gap-cum.gap%%multiple-cum.gap[1]
+            #Convert the above hours from date[1] to dates.
+           data$dates <- data$dates[1]+data$hour*60*60
+           
+             temp <- timeSeries(data$dates,"%Y-%m-%d %H:%M:%S",data[,8:length(data)])
+            data <- temp
+            }else{
+            	data <- data
+            	        
+          }}
+          
+          result<-aggregate(data[,8:length(data)],list(day=data$day,month=data$month,year=data$year,hour=data$hour),process,na.rm=na.rm)
+
+         data2 <- data.frame(Date=strptime(paste(00,result$hour,result$day,result$month,result$year),"%M %H %d %m %Y"),result[,5:length(result)])
+        sorted <- data2[order(data2$Date),]
+        final <- data.frame(Date=as.POSIXlt(sorted$Date),sorted[,2:length(sorted)])
+        names(final)<-c("Date",names(data[8:length(data)]))
+          return(final)
+        }
---
+++ R/timeSeries.R
@@ -0,0 +1,25 @@
+timeSeries<-
+	function(dates,
+			 dateformat,
+			 data=NULL){
+		if(class(data)=="numeric")data.length=length(data)else data.length=length(data[,1])
+		if(is.null(data)&&length(dates)!=data.length)
+			stop("Lengths differ between dates (",length(dates),") and data (",data.length, ").")
+			
+		dates<-(strptime(dates,dateformat))
+		minute<- as.numeric(format(dates,"%M"))
+		hour<- as.numeric(format(dates,"%H"))
+		day<-as.numeric(format(dates,"%d"))
+		week<-as.numeric(format(dates,"%W"))
+		month<-as.numeric(format(dates,"%m"))
+		year<-as.numeric(format(dates,"%Y"))
+
+		
+			results<-data.frame(dates,minute,hour,day,week,month,year,data)
+			return(results)
+		
+	}
+
+
+
+
