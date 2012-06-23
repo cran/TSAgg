81e5cd8dd4c7291145d392591e393d31680c020b
JS Lessels <jason.lessels@sydney.edu.au>
2012-06-23 00:00:00 +0000

    version 1.0.1

--- R/timeSeries.R
+++ R/timeSeries.R
@@ -1,31 +1,31 @@
-"timeSeries"<-function
+timeSeries<-function
 (dates, dateformat, data = NULL, tz = "GMT") 
 {
-	dimensions<-dim(data)
-	clss<-lapply(data,class)
-	if (!is.null(data)){
-		if (!is.null(dimensions[1])){
-			if (length(dates)!=length(data[,1]))
-				stop("Data and Date lengths differ")
-		}else{
-			if (length(dates)!=length(data))
-				stop("Data and Date lengths differ")
-		}
-	}	
-    dates <- (strptime(paste(dates), dateformat, tz = tz))
-    minute <- as.numeric(format(dates, "%M"))
-    hour <- as.numeric(format(dates, "%H"))
-    day <- as.numeric(format(dates, "%d"))
-    week <- as.numeric(format(dates, "%W"))
-    month <- as.numeric(format(dates, "%m"))
-    year <- as.numeric(format(dates, "%Y"))
-    if (is.null(data)) {
-        results <- data.frame(dates, minute, hour, day, week, 
-            month, year)
+  dimensions<-dim(data)
+  clss<-lapply(data,class)
+  if (!is.null(data)){
+    if (!is.null(dimensions[1])){
+      if (length(dates)!=length(data[,1]))
+        stop("Data and Date lengths differ")
+    }else{
+      if (length(dates)!=length(data))
+        stop("Data and Date lengths differ")
     }
-    else {
-        results <- data.frame(dates, minute, hour, day, week, 
-            month, year, data)
-    }
-    return(results)
+  }	
+  dates <- (strptime(paste(dates), dateformat, tz = tz))
+  minute <- minute(dates)
+  hour <- hour(dates)
+  day <- day(dates)
+  week <- week(dates)
+  month <- month(dates)
+  year <- year(dates)
+  if (is.null(data)) {
+    results <- data.frame(dates, minute, hour, day, week, 
+                          month, year)
+  }
+  else {
+    results <- data.frame(dates, minute, hour, day, week, 
+                          month, year, data)
+  }
+  return(results)
 }
